377ef2dc6d1413dc477808bfd2d7b873b729d819
facu2279 <2279@holbertonschool.com>
2020-09-16 17:03:39 +0000

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <iostream>
+#include <cstdlib> // to use rand()
+using std::cout;
+
+int main() {
+  cout << "Hey, we got ";
+  return 1;
+}
