efa1ba3d4b837d597a705d752fd3114baf2551dd
facu2279 <2279@holbertonschool.com>
2020-09-16 17:37:41 +0000

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main() {
-write("hola");
-  return 1;
+write(output,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+return (1);
 }
