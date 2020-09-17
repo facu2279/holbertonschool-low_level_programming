eee11223b423211e9435ece13ca3e11622479e6f
facu2279 <2279@holbertonschool.com>
2020-09-17 23:29:24 +0000

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,37 @@ int main(void)
 {
   int a, b, c, d, si;
   si = '0';
-  for (a = 48; a <= 57; a++){
-      for (b = 48; b <= 57; b++){
-	  for (c =48; c <= 57; c++){
-	      for (d = 48; d <= 57; d++){
-		  if ( a + b != c + d){
-		    if ( a + b < c + d){
-		      putchar(a);
-		      putchar(b);
-		      putchar(' ');
-		      putchar(c);
-		      putchar(d);
-		      putchar(',');
-		    }
-		  }
-		}
-	    }
+  for (a = '0'; a <= '9'; a++)
+    {
+      for (b = '0'; b <= '9'; b++)
+	{
+	  for (c = '0'; c <= '9'; c++)
+	    {
+	      for (d = '0'; d <= '9'; d++)
+               {
+		 if (a + b != c + d)
+		   {
+		     si = '1';
+		   }
+		 else if (a == '0' || b == '0')
+		       {
+			 if ( c == '0' || d == '0')
+			   {
+			     si = '1';
+			   }
+		       }
+		 if (si == '1')
+		   {
+		     putchar(a);
+		     putchar(b);
+		     putchar(44);
+		     putchar(c);
+		     putchar(d);
+		     putchar(32);
+		     si = '0';
+		   }
+	       }
+	   }
 	}
     }
   putchar('\n');
