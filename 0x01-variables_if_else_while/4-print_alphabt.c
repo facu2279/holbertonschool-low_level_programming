01d063bfd310d19d7e65294e19320ab0583c72c4
facu2279 <2279@holbertonschool.com>
2020-09-17 17:16:20 +0000

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,12 @@
 int main(void)
 {
 char ch;
-ch = 'a';
-while (ch != 'z')
+for (ch = 'a'; ch = 'z'; ch++)
 {
 if (ch != 'q' && ch != 'e')
 {
 putchar(ch);
 }
-ch++;
 }
 putchar('\n');
 return (0);
