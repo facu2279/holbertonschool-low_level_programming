bbcecee4ca4ff57779ac1dbb2c29c60488cf45ce
facu2279 <2279@holbertonschool.com>
2020-09-17 15:10:31 +0000

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int i=0;
+char letter="a"
+int main(void)
+{
+  for(i; i=26; i++)
+    {
+
+    } 
+return 0;
+}
