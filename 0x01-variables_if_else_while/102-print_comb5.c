4fdb2f963f867601affb65527283ec2c223b0580
facu2279 <2279@holbertonschool.com>
2020-09-18 00:24:05 +0000

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+B#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,32 +6,32 @@
  */
 int main(void)
 {
-  int a, b, c, d;
-  for (a = '0'; a <= '9'; a++)
-    {
-      for (b = '0'; b <= '9'; b++)
-	{
-	  for (c = '0'; c <= '9'; c++)
-	    {
-	      for (d = '0'; d <= '9'; d++)
-		{
-		  if ( a < c || (a == c && b < d))
-		    {
-		      putchar(a);
-		      putchar(b);
-		      putchar(' ');
-		      putchar(c);
-		      putchar(d);
-		      if (a != '9' || b != '8' || c != '9' || d != '9')
-			{
-		      putchar(',');
-		      putchar(' ');
-			}
-		    }
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int a, b, c, d;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+for (c = '0'; c <= '9'; c++)
+{
+for (d = '0'; d <= '9'; d++)
+{
+if (a < c || (a == c && b < d))
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+if (a != '9' || b != '8' || c != '9' || d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
