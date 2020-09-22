bd3b1a135e562ff066db3cb50aaba74bc2f6839e
facu2279 <2279@holbertonschool.com>
2020-09-22 14:29:33 +0000

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,32 +1,42 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * print_to_98  - print numbers n to 98
+ * @n: integer
+ *
+ * Return: print to stdout numbers n to 98 followed by a newline.
+ */
 void print_to_98(int n)
 {
-  int i;
-  if (n >= 98)
-    {
-      for (i = n; i >= 98; i--)
-	{
-	  printf("%i", i);
-	  if (i != 98)
-	    {
-	      printf(", ");
-	    }else{
-	    printf("\n");
-	  }
-	}
-    }
-  else
-    {
-    for (i = n; i <= 98; i++)
-      {
-	printf("%i", i);
-	if (i != 98)
-	 {
-	   printf(", ");
-	 }else{
-	  printf("\n");
-	}
-      }
-    }
+int i;
+if (n >= 98)
+{
+for (i = n; i >= 98; i--)
+{
+printf("%i", i);
+if (i != 98)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+}
+}
+else
+{
+for (i = n; i <= 98; i++)
+{
+printf("%i", i);
+if (i != 98)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+}
+}
 }
