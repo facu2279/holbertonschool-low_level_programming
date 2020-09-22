af4fb71db2838a031bc9d85fd0e5c7f8f7d58418
facu2279 <2279@holbertonschool.com>
2020-09-22 23:29:25 +0000

    a

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-int i, sum = 0;
+int i, a = 0;
 
 for (i = 0; i < 1024; i++)
 {
 if ((i % 3) == 0 || (i % 5) == 0)
-sum += i;
+a = a + i;
 }
 
-printf("%d\n", sum);
+printf("%d\n", a);
 
 return (0);
 }
