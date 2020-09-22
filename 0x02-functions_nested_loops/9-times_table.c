8248a77eb4fcc128a4c5ec8112fdbbb9b4f36a5e
facu2279 <2279@holbertonschool.com>
2020-09-22 13:56:15 +0000

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,51 +1,33 @@
 #include "holberton.h"
-void print(int);
+/**
+ * times_table - create a table of multiplication facts
+ *
+ * Return: print to stdout the 9 times table, starting with 0.
+ */
 void times_table(void)
 {
-  int i;
-  for(i = 0; i<= 9; i++)
-    {
-  print(i);
-  _putchar('\n');
-    }
-}
-void print(int multi)
+int a, b;
+for (a = 0; a <= 9; a++)
 {
-  int i,res,a,b;
-  for (i = 0; i <= 9; i++)
-    {
-      res=(i*multi);
-      if (res == 0)
-	{
-	  _putchar('0');
-	  if (i != 9)
-	    {
-	  _putchar(',');
-	  _putchar(' ');
-	  _putchar(' ');
-	    }
-	}
-      else if (res <= 9)
-	{
-	_putchar(res + '0');
-	if (i != 9)
-	  {
-	    _putchar(',');
-	    _putchar(' ');
-	    _putchar(' ');
-	  }
-	}
-      else
-	{
-	a = res / 10;
-	b = res % 10;
-	_putchar(a + '0');
-	_putchar(b + '0');
-	if (i != 9)
-	  {
-	    _putchar(',');
-	    _putchar(' ');
-	  }
-	}
-	}
+b = 0;
+_putchar('0');
+for (b = 1; b <= 9; b++)
+{
+if ((a * b) <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(a * b + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(a * b / 10 + '0');
+_putchar(a * b % 10 + '0');
+}
+}
+_putchar('\n');
+}
 }
