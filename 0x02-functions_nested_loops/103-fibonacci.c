f41bd8d83e8892652abf4d9898aa5bf0009e72cd
facu2279 <2279@holbertonschool.com>
2020-09-22 22:35:33 +0000

    a

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,24 +5,22 @@
  */
 int main(void)
 {
-  long int a, b, c;
+  long int a, b, c, total;
   a = 0;
   b = 1;
   c = 0;
+  total = 0;
   while (c <= 4000000)
     {
       c = a + b;
       if (c % 2 == 0)
 	{
-      printf("%ld", c);
-      if (c + b <= 4000000)
-	{
-	  printf(", ");
-	}
+	  total = total + c;
 	}
       a = b;
       b = c;
     }
+  printf("%lu",total);
   putchar('\n');
   return (0);
 }
