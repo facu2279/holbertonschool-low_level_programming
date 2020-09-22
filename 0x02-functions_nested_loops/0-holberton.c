b384725c7ff180048552cd7666a3270846ba5096
facu2279 <2279@holbertonschool.com>
2020-09-22 12:39:16 +0000

    a

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,17 @@
 #include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-char arr[] = {'H','o','l','b','e','r','t','o','n'};
+char arr[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 int i;
 for (i = 0; i < 9; i++)
 {
- _putchar(arr[i]);		    
+_putchar(arr[i]);
 }
- _putchar('\n');
+_putchar('\n');
 return (0);
 }
