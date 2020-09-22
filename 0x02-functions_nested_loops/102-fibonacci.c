50a076d7a91d51dbe23df1492fe9a1d1b3feb446
facu2279 <2279@holbertonschool.com>
2020-09-22 21:16:44 +0000

    a

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,26 @@
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+ * main - program to print fibonacci sequence (first 50)
+ * Return: 0
+ */
 int main(void)
 {
-  long int a, b, c, i;
-  a = 0;
-  b = 1;
- for (i = 0; i <= 49; i++)
-    {
-      c = a + b;
-      printf("%ld", c);
-      if (i != 49)
-	{
-      printf(", ");
-	}
-      a = b;
-      b = c;
-    }
- putchar('\n');
- return(0);
+long int a, b, c, i;
+a = 0;
+b = 1;
+for (i = 0; i <= 49; i++)
+{
+c = a + b;
+printf("%ld", c);
+if (i != 49)
+{
+printf(", ");
+}
+a = b;
+b = c;
+}
+putchar('\n');
+return (0);
 }
 
