0bf200143d063e6547e82b66fed6262bda5f528f
facu2279 <2279@holbertonschool.com>
2020-09-22 14:26:09 +0000

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,57 +1,32 @@
 #include "holberton.h"
+#include <stdio.h>
 void print_to_98(int n)
 {
-  int a,b,i,c;
+  int i;
   if (n >= 98)
     {
       for (i = n; i >= 98; i--)
 	{
-	  a = i / 100;
-	  b = (i / 10) % 10;
-	  c = i % 10;
-	  if (a != 0)
-	    {
-	      _putchar(a + '0');
-	    }
-	  _putchar(b + '0');
-	  _putchar(c + '0');
+	  printf("%i", i);
 	  if (i != 98)
 	    {
-	      _putchar(',');
-	      _putchar(' ');
-	    }
-
-
+	      printf(", ");
+	    }else{
+	    printf("\n");
+	  }
 	}
     }
   else
     {
     for (i = n; i <= 98; i++)
       {
-	if (i < 0)
-	  {
-	    a = ((-1) * (i / 10));
-	    b = ((-1) * (i % 10));
-	    _putchar('-');
-	    _putchar(a + '0');
-	    _putchar(b + '0');
-	  }
-	else
-	  {
-	  a = (i / 10);
-	  b = (i % 10);
-	  if (a != 0)
-	    {
-	      _putchar(a + '0');
-	    }
-	  _putchar(b + '0');
-	  }
-    if (i != 98)
+	printf("%i", i);
+	if (i != 98)
 	 {
-	   _putchar(',');
-	   _putchar(' ');
-	 }
+	   printf(", ");
+	 }else{
+	  printf("\n");
+	}
       }
     }
-  _putchar('\n');
 }
