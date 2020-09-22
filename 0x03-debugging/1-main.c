3e4266352415c679ed4f3b8928f0345e4d8227d2
facu2279 <2279@holbertonschool.com>
2020-09-22 17:24:55 +0000

    a

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
-while (i < /*1*/0)
-{
-putchar(i);
-}
-
+/*
+*while (i < 10)
+ *{
+ *putchar(i);
+ *}
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
