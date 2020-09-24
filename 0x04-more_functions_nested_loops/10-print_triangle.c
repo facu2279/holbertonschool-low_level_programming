34ac8a105268dbf61a210d5ae21e73091b428c80
facu2279 <2279@holbertonschool.com>
2020-09-24 17:03:28 +0000

    a

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,27 +5,29 @@
  */
 void print_triangle(int size)
 {
-  int a, b, x;
-  x = size - 1;
-  if (size > 0)
-    {
-      for (a = 1; a <= size; a++)
-	{
-	  for (b = 1; b <= size; b++)
-	    {
-	      if (b <= x)
-		{
-		  _putchar(' ');
-		}else{
-		_putchar('#');
-	      }
-	    }
-	  x = x - 1;
-	  _putchar('\n');
-	}
-    }
-  else
-    {
-      _putchar('\n');
-    }
+int a, b, x;
+x = size - 1;
+if (size > 0)
+{
+for (a = 1; a <= size; a++)
+{
+for (b = 1; b <= size; b++)
+{
+if (b <= x)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+x = x - 1;
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
