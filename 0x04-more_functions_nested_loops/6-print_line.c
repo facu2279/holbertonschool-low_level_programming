dc615ba24cfe5a1eda4ac1557d474dd7a90e4d10
facu2279 <2279@holbertonschool.com>
2020-09-24 15:43:34 +0000

    a

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,27 @@
 #include "holberton.h"
-
+/**
+ * print_line - print numbers from 0 to 14
+ * @n: integer
+ */
 void print_line(int n)
 {
-  int a;
-  if (n >= 0)
-    {
-  for (a = n; a > 0; a--)
-    {
-      if ( a <= 1)
-	{
-	 _putchar('\n');
-	}else{
-	  _putchar('_');
-	}
-    }
-    }else{
-    _putchar('\n');
-  }
+int a;
+if (n > 0)
+{
+for (a = n; a > 0; a--)
+{
+if (a <= 1)
+{
+_putchar('\n');
+}
+else
+{
+_putchar('_');
+}
+}
+}
+else
+{
+_putchar('\n');
+}
 }
