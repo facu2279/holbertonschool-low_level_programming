eab1970e59193367ad02d112813703780e89c256
facu2279 <2279@holbertonschool.com>
2020-09-24 15:28:24 +0000

    a

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * _mul- multiplication
+ * mul- multiplication
  * @a: integer
  * @b: integer
- * Return: return multiplication between a and b 
+ * Return: return multiplication between a and b
  */
 int mul(int a, int b)
 {
