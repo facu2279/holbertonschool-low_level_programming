a9c36bad7deca73303dc0d65c4622a6cf7f53b4c
facu2279 <2279@holbertonschool.com>
2020-09-24 15:34:54 +0000

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,22 +5,24 @@
  */
 void more_numbers(void)
 {
-  int i, i2, a;
+int i, i2, a;
 
-  for (i2 = 1; i2 <=10; i2++)
-    {
- for (i = 0; i < 15; i++)
-    {
-      if (i < 10)
-	{
-	  a = i;
-	}else{
-	a = i / 10;
-	_putchar(a + '0');
-	a = i % 10;
-      }
-      _putchar(a + '0');
-    }
-  _putchar('\n');
+for (i2 = 1; i2 <= 10; i2++)
+{
+for (i = 0; i < 15; i++)
+{
+if (i < 10)
+{
+a = i;
+}
+else
+{
+a = i / 10;
+_putchar(a + '0');
+a = i % 10;
+}
+_putchar(a + '0');
+}
+_putchar('\n');
 }
 }
