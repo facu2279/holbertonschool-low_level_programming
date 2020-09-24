8c0cf3cec55cb911a460dfee26db92d4bd195183
facu2279 <2279@holbertonschool.com>
2020-09-24 18:53:30 +0000

    a

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,25 +5,25 @@
  */
 int main(void)
 {
-  unsigned long int n, div, maxfact;
-  div = 2;
-  n = 612852475143;
-  while (n != 0)
-    {
-      if ((n % div) != 0)
-	{
-	  div++;
-	}
-      else
-	{
-	  maxfact = n;
-	  n = n / div;
-	  if (n == 1)
-	    {
-	      printf("%lu\n", maxfact);
-	      break;
-	    }
-	}
-    }
-  return (0);
+unsigned long int n, div, maxfact;
+div = 2;
+n = 612852475143;
+while (n != 0)
+{
+if ((n % div) != 0)
+{
+div++;
+}
+else
+{
+maxfact = n;
+n = n / div;
+if (n == 1)
+{
+printf("%lu\n", maxfact);
+break;
+}
+}
+}
+return (0);
 }
