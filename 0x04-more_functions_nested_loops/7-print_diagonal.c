9c8d9d27b656460c66f2d6cc9ca106a66fec66c3
facu2279 <2279@holbertonschool.com>
2020-09-24 15:56:35 +0000

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
-void spaces(int c);
+/**
+ * print_diagonal - print numbers from 0 to 14
+ * @n: integer
+ */
 void print_diagonal(int n)
 {
-  int a;
-  if (n > 0)
-    {
-      for (a = 1; a <= n; a++)
-	{
-	  spaces(a -1);
-	  _putchar('\\');
-	  _putchar('\n');
-	}
-    }else{
-
-  _putchar('\n');
-  }
-}
-void spaces(int c)
+int a, b, i;
+if (n > 0)
 {
-  int a;
-  for (a = 1; a <= c; a++)
-    {
-      _putchar(' ');
-    }
+for (a = 1; a <= n; a++)
+{
+b = a - 1;
+for (i = 1; i <= b; i++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
