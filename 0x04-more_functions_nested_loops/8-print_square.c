879f2b20ab741c64de64f8e67472f32a83b8c036
facu2279 <2279@holbertonschool.com>
2020-09-24 16:00:15 +0000

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,18 +1,24 @@
 #include "holberton.h"
-
+/**
+ * print_square - print numbers from 0 to 14
+ * @size: integer
+ */
 void print_square(int size)
 {
-  int a, b;
-  if (size > 0)
-    {
-      for (a = 1; a <= size; a++)
-        {
-          for (b = 1; b <= size; b++)
-            {
-              _putchar('#');                                                                    }                                                                                 _putchar('\n');
-        }                                                                               }else{
-
-    _putchar('\n');
-  }
-
+int a, b;
+if (size > 0)
+{
+for (a = 1; a <= size; a++)
+{
+for (b = 1; b <= size; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
