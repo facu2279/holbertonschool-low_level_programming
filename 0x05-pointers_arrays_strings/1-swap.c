1822727c2c03264cf6ca3ae52061f6c22ffc5663
facu2279 <2279@holbertonschool.com>
2020-09-29 12:44:55 +0000

    a

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
+/**
+ * swap_int - asdf
+ * @a: asda
+ * @b: sdfsd
+ */
 void swap_int(int *a, int *b)
 {
-  int c, d;
-  c = *a;
-  d = *b;
-  *a = d;
-  *b = c;
-
+int c, d;
+c = *a;
+d = *b;
+*a = d;
+*b = c;
 }
