5b3a2a8c78114014f673e16f7c190ccb82acdaf5
facu2279 <2279@holbertonschool.com>
2020-09-29 12:53:22 +0000

    a

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,17 @@
 #include "holberton.h"
+/**
+ * print_rev - fas
+ * @s: sdsdf
+ */
 void print_rev(char *s)
 {
-  int i, b;
-  for (i = 0; s[i] != '\0'; i++)
-    {
-    }
-  for (b = i-1; b >= 0; b--)
-    {
-      _putchar(s[b]);
-    }
-  _putchar('\n');
+int i, b;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+for (b = i - 1; b >= 0; b--)
+{
+_putchar(s[b]);
+}
+_putchar('\n');
 }
