ce71c3969e31df802284d29c61b6414719ab3206
facu2279 <2279@holbertonschool.com>
2020-09-29 16:58:39 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,12 +6,12 @@
  */
 int _atoi(char *s)
 {
-  int i, a, b = 0, res = 0, negativo = 0;
+int i, a, b = 0, res = 0, negativo = 0;
 for (a = 0; s[a] != '\0'; a++)
 {
 if (s[a] >= 0 && s[a] <= 9)
 {
-if(s[a - 1] == '0')
+if (s[a - 1] == '0')
 {
 negativo = 1;
 }
@@ -33,6 +33,6 @@ return (res);
 }
 else
 {
-return(res * (-1));
+return (res * (-1));
 }
 }
