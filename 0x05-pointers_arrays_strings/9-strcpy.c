ba917c9215078d42ee198d426c561676624db518
facu2279 <2279@holbertonschool.com>
2020-09-29 15:50:36 +0000

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
-char *_strcpy(char *dest, char *src){
-  int i;
-  for (i = 0; src[i] >= '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-  return(dest);
+/**
+ * *_strcpy - sdgdfgs
+ * @dest: sfdgsdf
+ * @src: gdsfgsd
+ * Return: sdfgsdfg
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] <= '\0'; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
 }
