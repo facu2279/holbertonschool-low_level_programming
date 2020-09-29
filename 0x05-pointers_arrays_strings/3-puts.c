563691763d9fd2f531daa81fd5f423e2fff71aad
facu2279 <2279@holbertonschool.com>
2020-09-29 12:51:25 +0000

    a

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
+/**
+ * _puts - asdas
+ * @str: sdfsdf
+ */
 void _puts(char *str)
 {
-  int i;
-  for (i = 0; str[i] != '\0'; i++)
-    {
-      _putchar(str[i]);
-    }
-  _putchar('\n');
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
