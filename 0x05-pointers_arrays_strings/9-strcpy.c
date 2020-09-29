be84b4c9f8a801c78bfcdb26dc272be756a5aa67
facu2279 <2279@holbertonschool.com>
2020-09-29 15:58:07 +0000

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,10 +7,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int i;
-for (i = 0; src[i] != '\0'; i++)
+  int i, i2;
+  for(i = 0; src[i] != '\0'; i++)
+    {
+
+    }
+for (i2 = 0; i2 <= i; i2++)
 {
-dest[i] = src[i];
+dest[i2] = src[i2];
 }
 return (dest);
 }
