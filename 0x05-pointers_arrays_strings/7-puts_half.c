2ff101fc251954719826d22298d900d06c2e2c3e
facu2279 <2279@holbertonschool.com>
2020-09-29 13:08:13 +0000

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,21 @@
 #include "holberton.h"
+/**
+ * puts_half - sfdfs
+ * @str: fsadf
+ */
 void puts_half(char *str)
 {
-  int i, a;
-  for(i = 0; str[i] != '\0'; i++)
-    {
-    }
-  if(i % 10 != 0)
-    {
-      i = i-1;
-    }
-  for (a = i / 2; a <= i; a++)
-    {
-      _putchar(str[a]);
-    }
-  _putchar('\n');
+int i, a;
+for(i = 0; str[i] != '\0'; i++)
+{
+}
+if(i % 10 != 0)
+{
+i = i - 1;
+}
+for (a = i / 2; a <= i; a++)
+{
+_putchar(str[a]);
+}
+_putchar('\n');
 }
