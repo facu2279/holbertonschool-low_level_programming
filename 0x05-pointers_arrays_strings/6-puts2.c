b6e89d4b60d52efac4cb3dcffca5c47b0ea0a1de
facu2279 <2279@holbertonschool.com>
2020-09-29 14:19:22 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,10 +5,10 @@
  */
 void puts2(char *str)
 {
-  int i, i2;
-  for (i = 0; str[i] != '\0'; i++)
-    {
-    }
+int i, i2;
+for (i = 0; str[i] != '\0'; i++)
+{
+}
 for (i2 = 0; i2 < i; i2 += 2)
 {
 _putchar(str[i2]);
