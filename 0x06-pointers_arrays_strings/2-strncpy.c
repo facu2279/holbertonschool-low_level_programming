d49a1a18e9dead0870e93b1c3df1ac8ca5f17414
facu2279 <2279@holbertonschool.com>
2020-10-01 16:28:26 +0000

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,25 @@
 #include "holberton.h"
 /**
- *_strncpy - asdf
- *@dest: sadf
- *@src: sdfsad
- *@n: sdfas
- *Return: sfd
+ * char *_strncpy - function that copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: max byte of src from dest
+ * Return: return dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i, i2;
-for (i = 0; src[i] != '\0'; i++)
-{
-}
-if (n > i)
-{
-n = i;
-}
-for (i2 = 0; i2 < n; i2++)
-{
-dest[i2] = src[i2];
-}
-dest[i] = '\0';
-return (dest);
+  int i, a = 0;
+  for (i = 0; src[i] != '\0'; i++)
+    {
+    }
+  if (n > i)
+    {
+      n = i;
+    }
+  while (a <= n)
+    {
+      dest[a] = src[a];
+      a++;
+    }
+  return (dest);
 }
