bbec239f900085fa262467b3bbcb9981b8212bc6
facu2279 <2279@holbertonschool.com>
2020-10-01 13:11:24 +0000

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_strncat - sdfa
+ *@dest: sad
+ *@src: sds
+ *@n:sdf
+ *Return: sdf
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+for(i = 0; dest[i] != '\0'; i++)
+{
+}
+for (i2 = i, i = 0; i2 <= largo && i <= n; i2++)
+{
+dest[i2] = src[i];
+i++;
+}
+return (dest);
+}
