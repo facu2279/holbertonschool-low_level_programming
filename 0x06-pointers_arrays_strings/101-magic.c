fc79c781768a11d4fed8945625c7d6002ae2e2e5
facu2279 <70913784+facu2279@users.noreply.github.com>
2020-10-01 23:35:13 -0300

    Update 101-magic.c

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -1,46 +1,23 @@
 #include <stdio.h>
-#include "holberton.h"
-/**
- * print_buffer - check the code for Holberton School students.
- * @b: pinter to int to be determined.
- * @size: int.
- * Return: void.
- */
-void print_buffer(char *b, int size)
+
+int main(void)
 {
-	if (size > 0)
-	{
-		int cont = 0, i, i2;
-		int p = 0;
-		for (i = 0; i < size; i += 10)
-		{
-			printf("%08x: ", cont);
-			printf("%02x%02x ", b[i], b[i + 1]);
-			printf("%02x%02x ", b[i + 2], b[i + 3]);
-			printf("%02x%02x ", b[i + 4], b[i + 5]);
-			printf("%02x%02x ", b[i + 6], b[i + 7]);
-			printf("%02x%02x ", b[i + 8], b[i + 9]);
-			for (i2 = cont; i2 < cont + 10; i2 += 10)
-			{
-				for (p = 0; p < 10; p++)
-				{
-					if (b[i + p] >= 0 && b[i + p] <= 31)
-					{
-						putchar(46);
-					}
-					else
-						putchar(b[i+ p]);
-				}
+  int n;
+  int a[5];
+  int *p;
 
-			}
-
-			cont = cont + 10;
-			if(i % 10 == 0)
-			{
-				putchar('\n');
-			}
-		}
-	}
-	else
-		printf("\n");
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
