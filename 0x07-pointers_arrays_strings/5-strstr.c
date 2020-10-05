817222490d4f63c04dfc7d4e48d82952c62a7b33
facu2279 <2279@holbertonschool.com>
2020-10-05 17:04:53 +0000

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * _strstr - dfgsd
  * @haystack: sdfa
  * @needle: afa
- * Return : sadf
+ * Return: sadf
  */
 char *_strstr(char *haystack, char *needle)
 {
-  int i, i2;
-  for (i = 0; haystack[i] != '\0'; i++)
-    {
-      for (i2 = 0; needle[i2] != '\0'; i2++)
-	{
-	  if(haystack[i] == needle[i2])
-	    {
-	      haystack = haystack + i + i2 + 2;
-	      return(haystack);
-	    }
-	}
-    }
-  return (NULL);
+int i, i2;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (i2 = 0; needle[i2] != '\0'; i2++)
+{
+if (haystack[i] == needle[i2])
+{
+haystack = haystack + i + i2 + 2;
+return (haystack);
+}
+}
+}
+return ('\0');
 }
