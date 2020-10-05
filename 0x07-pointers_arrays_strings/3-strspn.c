d9c6c4e048bfe0570ddbe516db2a485376f8606f
facu2279 <2279@holbertonschool.com>
2020-10-05 20:23:18 +0000

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 /**
- * _strspn - sdfs
- * @s: asfasd
- * @accept: safas
- * Return: adfasd
+ * _strspn - gets length of prefix substring
+ * @s: string
+ * @accept: accept string
+ * Return: count of accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int i, i2, cont;
+  int i, j, k = 0;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-for (i2 = 0; accept[i2] != '\0'; i2++)
-{
-if (s[i] == accept[i2])
-{
-cont = cont + 1;
-break;
-}
-}
-if (s[i] != accept[i2])
-break;
-}
-return (cont);
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      for (j = 0; accept[j] != '\0'; j++)
+	{
+	  if (s[i] == accept[j])
+	    {
+	      k++;
+	      break;
+	    }
+	}
+      if (s[i] != accept[j])
+	break;
+    }
+  return (k);
 }
