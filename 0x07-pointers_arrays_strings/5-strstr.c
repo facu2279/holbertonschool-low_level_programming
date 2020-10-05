34f26d3171dd8c3128411999f1aaa55616742adc
facu2279 <2279@holbertonschool.com>
2020-10-05 17:16:41 +0000

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,15 +9,15 @@ char *_strstr(char *haystack, char *needle)
 {
 int i, i2;
 
-for (i = 0; haystack[i] != '\0'; i++)
-{
-for (i2 = 0; needle[i2] != '\0'; i2++)
-{
-if (haystack[i] == needle[i2])
-{
-return (haystack + i + i2 + 2);
-}
-}
-}
-return ('\0');
+ for (i = 0; haystack[i] != '\0'; i++)
+   {
+     for (i2 = 0; needle[i2] != '\0'; i2++)
+       {
+	 if (haystack[i] != needle[i2])
+	   {
+	     return (needle + i);
+	   }
+       }
+   }
+ return ('\0');
 }
