8cc0046496b082e08b1ed233c82ddae62f728dfe
facu2279 <2279@holbertonschool.com>
2020-10-05 20:25:37 +0000

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,20 +7,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  int i, i2, cont = 0;
+int i, i2, cont = 0;
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (i2 = 0; accept[i2] != '\0'; i2++)
-	{
-	  if (s[i] == accept[i2])
-	    {
-	      cont++;
-	      break;
-	    }
-	}
-      if (s[i] != accept[i2])
-	break;
-    }
-  return (cont);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (i2 = 0; accept[i2] != '\0'; i2++)
+{
+if (s[i] == accept[i2])
+{
+cont++;
+break;
+}
+}
+if (s[i] != accept[i2])
+break;
+}
+return (cont);
 }
