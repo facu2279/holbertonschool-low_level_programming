c9f36a5bc8c63f72f7c43f056dc764c720010eae
facu2279 <2279@holbertonschool.com>
2020-10-05 18:05:24 +0000

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,16 +5,16 @@
  */
 void print_chessboard(char (*a)[8])
 {
-  int i, i2;
-  for (i = 0; i <= 7; i++)
-    {
-      for (i2 = 0; i2 <= 7; i2++)
-	{
-	  _putchar(a[i][i2]);
-	  if (i2 == 7 && i < 8)
-	    {
-	      _putchar('\n');
-	    }
-	}
-    }
+int i, i2;
+for (i = 0; i <= 7; i++)
+{
+for (i2 = 0; i2 <= 7; i2++)
+{
+_putchar(a[i][i2]);
+if (i2 == 7 && i < 8)
+{
+_putchar('\n');
+}
+}
+}
 }
