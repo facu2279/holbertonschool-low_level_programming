0480f1c83615a8e789d77a1ec6fc65dffaeb4846
facu2279 <2279@holbertonschool.com>
2020-10-05 19:13:55 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,14 +7,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-  int i, diag1 = 0, diag2 = 0;
-  for (i = 0; i <= size*size; i = i + size + 1)
-    {
-      diag1 = diag1 + *(a + i);
-    }
-  for (i = size - 1; i <= size*size - 2; i = i + size - 1)
-    {
-      diag2 = diag2 + *(a + i);
-    }
-  printf("%d, %d\n",diag1,diag2);
+int i, diag1 = 0, diag2 = 0;
+
+for (i = 0; i <= size * size; i = i + size + 1)
+{
+diag1 = diag1 + *(a + i);
+}
+for (i = size - 1; i <= size * size - 2; i = i + size - 1)
+{
+diag2 = diag2 + *(a + i);
+}
+printf("%d, %d\n", diag1, diag2);
 }
