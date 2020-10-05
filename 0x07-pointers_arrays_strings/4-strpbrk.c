11a6c5423c96542633246fe43723f94b6d6f0c73
facu2279 <2279@holbertonschool.com>
2020-10-05 15:24:16 +0000

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,18 +7,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-  int i, i2;
+int i, i2;
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (i2 = 0; accept[i2] != '\0'; i2++)
-	{
-	  if (s[i] == accept[i2])
-	    {
-	      return (s + i);
-	    }
-	}
-    }
-
-  return ('\0');
+for (i = 0; s[i] != '\0'; i++)
+{
+for (i2 = 0; accept[i2] != '\0'; i2++)
+{
+if (s[i] == accept[i2])
+{
+return (s + i);
+}
+}
+}
+return ('\0');
 }
