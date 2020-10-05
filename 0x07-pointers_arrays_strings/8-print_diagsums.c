5351c36fcd4d9a0931d1e25393282ef624c49aa7
facu2279 <2279@holbertonschool.com>
2020-10-05 19:12:18 +0000

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - dsfas
+ * @a: asdfd
+ * @size: asdfas
+ */
+void print_diagsums(int *a, int size)
+{
+  int i, diag1 = 0, diag2 = 0;
+  for (i = 0; i <= size*size; i = i + size + 1)
+    {
+      diag1 = diag1 + *(a + i);
+    }
+  for (i = size - 1; i <= size*size - 2; i = i + size - 1)
+    {
+      diag2 = diag2 + *(a + i);
+    }
+  printf("%d, %d\n",diag1,diag2);
+}
