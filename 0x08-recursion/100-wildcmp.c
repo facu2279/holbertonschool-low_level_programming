5aba248afd634f3e4f4c61628b9277d06c0b04ed
facu2279 <70913784+facu2279@users.noreply.github.com>
2020-10-07 22:59:57 -0300

    Update 100-wildcmp.c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,5 +1,22 @@
 #include "holberton.h"
-int wildcmp(char *s1, char *s2);
+/**
+  * wildcmp - edasss 
+  * @s1: redsddd 
+  * @s2: htdess 
+  * Return: edss 
+  */
+int wildcmp(char *s1, char *s2)
 {
-  return (1);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
 }
