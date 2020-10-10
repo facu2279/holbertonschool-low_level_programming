66766224f24416c743745623eff608e493dff250
facu2279 <70913784+facu2279@users.noreply.github.com>
2020-10-10 16:31:18 -0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,52 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - fdghdfg
- * @argc: fdghfdghdfg
- * @argv: dfghdfghfdgh
- * Return: fdgsdfg
- */
+* main - eassadf
+* @argc: asdfasdf
+* @argv: asdfasdfsa
+* Return: sadfasdf
+*/
 int main(int argc, char *argv[])
 {
-int cambio, monedas = 0;
+	int cambio, monedas = 0;
 
- if (argc == 2)
-   {
-     cambio = atoi(argv[1]);
-     while (cambio > 0)
-       {
-	 if (cambio >= 25)
-	   {
-	     cambio = cambio - 25;
-	     monedas++;
-	   }
-	 else if ( cambio >= 10)
-	   {
-	     cambio = cambio - 10;
-	     monedas++;
-	   }
-	 else if (cambio >= 5)
-	   {
-	     cambio = cambio - 5;
-	     monedas++;
-	   }
-	 else if (cambio >= 2)
-	   {
-	     cambio = cambio - 2;
-	     monedas++;
-	   }
-	 else if (cambio >= 1)
-	   {
-	     cambio = cambio - 1;
-	     monedas++;
-	   }
-       }
-     printf("%d\n", monedas);
-     return (0);
-   }
- else
-   {
- printf("Error \n");
- return (1);
-   }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cambio = atoi(argv[1]);
+	while (cambio > 0)
+	{
+		if (cambio >= 25)
+		{
+			cambio = cambio - 25;
+			monedas++;
+		}
+		else if (cambio >= 10)
+		{
+			cambio = cambio - 10;
+			monedas++;
+		}
+		else if (cent >= 5)
+		{
+			cambio = cambio - 5;
+			monedas++;
+		}
+		else if (cambio >= 2)
+		{
+			cambio = cambio - 2;
+			monedas++;
+		}
+		else
+		{
+			cambio = cambio - 1;
+			monedas += 1;
+		}
+	}
+	printf("%d\n", monedas);
+	return (0);
 }
