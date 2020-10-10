e8c213d6d1463039556ac8ca2607d5ca936f6507
facu2279 <2279@holbertonschool.com>
2020-10-10 19:21:48 +0000

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,43 +8,40 @@
  */
 int main(int argc, char *argv[])
 {
-int cambio, monedas;
+int cambio, monedas = 0;
 
-if (argc == 2)
-{
-cambio = atoi(argv[1]);
-monedas = 0;
-while (cambio > 0)
-{
-if (cambio >= 25)
-{
-cambio = cambio - 25;
-monedas++;
-}
-else if (cambio >= 10)
-{
-cambio = cambio - 10;
-monedas++;
-}
-else if (cambio >= 5)
-{
-cambio = cambio - 5;
-monedas++;
-}
-else if (cambio >= 2)
-{
-cambio = cambio - 2;
-monedas++;
-}
-else
-{
-cambio--;
-monedas++;
-}
-}
-printf("%d\n", monedas);
-return (0);
-}
-printf("Error\n");
-return (1);
+ if (argc == 2)
+   {
+     cambio = atoi(argv[1]);
+     while (cambio > 0)
+       {
+	 if (cambio >= 25)
+	   {
+	     cambio = cambio - 25;
+	     monedas++;
+	   }
+	 else if ( cambio >= 10)
+	   {
+	     cambio = cambio - 10;
+	     monedas++;
+	   }
+	 else if (cambio >= 5)
+	   {
+	     cambio = cambio - 5;
+	     monedas++;
+	   }
+	 else if (cambio >= 2)
+	   {
+	     cambio = cambio - 2;
+	     monedas++;
+	   }
+	 else if (cambio >= 1)
+	   {
+	     cambio = cambio - 1;
+	     monedas++;
+	   }
+       }
+   }
+ printf("Error \n");
+ return (1);
 }
