3d894d9e6c2de01e5b5e79813740e995254f4ad1
facu2279 <2279@holbertonschool.com>
2020-10-10 15:08:28 +0000

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-  int i = 0;
-  if (i == 0)
-    {
-      argc = argc;
-    }
-  printf("%s \n", *argv);
-  return (0);
+(void) argc;
+printf("%s \n", *argv[0]);
+return (0);
 }
