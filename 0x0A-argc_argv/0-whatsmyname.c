b893109748a368b183a5c44368fd14e393b759fd
facu2279 <2279@holbertonschool.com>
2020-10-10 15:13:51 +0000

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 /**
  * main - sadfa
@@ -9,6 +8,6 @@
 int main(int argc, char *argv[])
 {
 argc = 0;
- printf("%s\n", argv[argc]);
+printf("%s\n", argv[argc]);
 return (0);
 }
