cd283da2b61fb2518516474a8b43ceb70f47e94a
facu2279 <2279@holbertonschool.com>
2020-10-10 15:27:02 +0000

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,15 @@
  */
 int main(int argc, char *argv[])
 {
-  int mul = 1;
+int mul = 1;
 
-  if (argc == 3)
-    {
-      mul *= atoi(argv[1]);
-      mul *= atoi(argv[2]);
-      printf("%d\n", mul);
-    }
-  else
-    printf("Error\n");
-  return (0);
-
+if (argc == 3)
+{
+mul *= atoi(argv[1]);
+mul *= atoi(argv[2]);
+printf("%d\n", mul);
+}
+else
+printf("Error\n");
+return (0);
 }
