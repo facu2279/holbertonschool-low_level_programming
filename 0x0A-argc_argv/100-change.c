59a2a15a10de16ea4fbfc52f6dc093537caa4b8e
facu2279 <70913784+facu2279@users.noreply.github.com>
2020-10-10 16:37:36 -0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,43 +8,42 @@
 */
 int main(int argc, char *argv[])
 {
-	int cambio, monedas = 0;
+int cambio, monedas = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	cambio = atoi(argv[1]);
-	while (cambio > 0)
-	{
-		if (cambio >= 25)
-		{
-			cambio = cambio - 25;
-			monedas++;
-		}
-		else if (cambio >= 10)
-		{
-			cambio = cambio - 10;
-			monedas++;
-		}
-		else if (cambio >= 5)
-		{
-			cambio = cambio - 5;
-			monedas++;
-		}
-		else if (cambio >= 2)
-		{
-			cambio = cambio - 2;
-			monedas++;
-		}
-		else
-		{
-			cambio--;
-			monedas++;
-		}
-	}
-	printf("%d\n", monedas);
-	return (0);
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cambio = atoi(argv[1]);
+while (cambio > 0)
+{
+if (cambio >= 25)
+{
+cambio = cambio - 25;
+monedas++;
+}
+else if (cambio >= 10)
+{
+cambio = cambio - 10;
+monedas++;
+}
+else if (cambio >= 5)
+{
+cambio = cambio - 5;
+monedas++;
+}
+else if (cambio >= 2)
+{
+cambio = cambio - 2;
+monedas++;
+}
+else
+{
+cambio--;
+monedas++;
+}
+}
+printf("%d\n", monedas);
+return (0);
 }
