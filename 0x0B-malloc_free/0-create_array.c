4699aca13b51a4b9c2b0234d0c82af562810fa20
facu2279 <2279@holbertonschool.com>
2020-10-14 16:57:50 +0000

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,31 +4,31 @@
  * create_array - sfgs
  *@size: sfsa
  *@c: asdfd
- *Return : sdfg
+ *Return: sdfg
  */
 char *create_array(unsigned int size, char c)
 {
-  char *ar;
-  unsigned int i = 0;
+char *ar;
+unsigned int i = 0;
 
-  if (size <= 0)
-    {
-      return (NULL);
-    }
-  else
-    {
-      ar = (char*)malloc(size);
-      if (ar != NULL)
-	{
-	  for (i = 0; i < size; i++)
-	    {
-	      ar[i] = c;
-	    }
-	  return (ar);
-	}
-      else
-	{
-	  return (NULL);
-	}
-    }
+if (size <= 0)
+{
+return (NULL);
+}
+else
+{
+ar = (char *)malloc(size);
+if (ar != NULL)
+{
+for (i = 0; i < size; i++)
+{
+ar[i] = c;
+}
+return (ar);
+}
+else
+{
+return (NULL);
+}
+}
 }
