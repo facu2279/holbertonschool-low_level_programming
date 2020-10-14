18c34280b6bfdf7393f6011ff8eaaba9594e6b0c
facu2279 <2279@holbertonschool.com>
2020-10-14 20:48:09 +0000

    0x0B

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,8 +1,15 @@
 #include "holberton.h"
 #include <stdlib.h>
+/**
+ * free_grid - sdafsa
+ *@grid: sadf
+ *@height: sadf
+ *Return: sadf
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
