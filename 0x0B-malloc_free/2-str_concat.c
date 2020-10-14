9573eec2c8a2622ae5dc3f4fab0909a117d403ca
facu2279 <2279@holbertonschool.com>
2020-10-14 17:54:08 +0000

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,23 +10,16 @@ char *str_concat(char *s1, char *s2)
 {
   char *ar;
   int largo1, largo2, largototal, i , j, k;
-  if (s1 != NULL && s2 != NULL)
-    {
+if (s1 == NULL)
+s1 = "";
+if(s2 == NULL)
+s2 = "";
       for (largo1 = 0; s1[largo1] != '\0'; largo1++)
 	{
 	}
       for (largo2 = 0; s2[largo2] != '\0'; largo2++)
 	{
 	}
-    }
-  else
-    {
-      if (s1 == NULL)
-	s1 = "";
-      if (s2 == NULL)
-	s2 = "";
-      return (NULL);
-    }
   
   largototal = largo1 + largo2;
   ar = (char *)malloc(largototal + 1);
