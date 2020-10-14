ad4c3d18d58f82bef50799889f8fbe6bcb83bcad
facu2279 <2279@holbertonschool.com>
2020-10-14 19:44:34 +0000

    0x0B

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - allocates a matrix, make space and free space
+ * @width: takes in width of matrix
+ * @height: height of matrix
+ * Return: matrix with freed spaces
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(sizeof(int *) * height);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; j < width; j++)
+		arr[i][j] = 0;
+	return (arr);
+}
