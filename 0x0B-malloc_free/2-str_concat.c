148125fc89ea490016bd457fc332c9b7f38ab1df
facu2279 <2279@holbertonschool.com>
2020-10-14 18:06:40 +0000

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,41 +4,41 @@
  * str_concat - fdg
  * @s1: sdf
  * @s2: sadfa
- * Return : 
+ * Return:
  */
 char *str_concat(char *s1, char *s2)
 {
-  char *ar;
-  int largo1, largo2, largototal, i , j, k;
+char *ar;
+int largo1, largo2, largototal, i, j, k;
+
 if (s1 == NULL)
 s1 = "";
-if(s2 == NULL)
+if (s2 == NULL)
 s2 = "";
-      for (largo1 = 0; s1[largo1] != '\0'; largo1++)
-	{
-	}
-      for (largo2 = 0; s2[largo2] != '\0'; largo2++)
-	{
-	}
-  
-  largototal = largo1 + largo2;
-  ar = (char *)malloc(largototal + 1);
-  if (ar != NULL)
-    {
-      for (i = 0; i < largo1; i++)
-	{
-	  ar[i] = s1[i];
-	}
-      k = 0;
-      for (j = i; j <= largototal; j++)
-	{
-	  ar[j] = s2[k];
-	  k++;
-	}
-      return (ar);
-    }
-  else
-    {
-      return (NULL);
-    }
+for (largo1 = 0; s1[largo1] != '\0'; largo1++)
+{
+}
+for (largo2 = 0; s2[largo2] != '\0'; largo2++)
+{
+}
+largototal = largo1 + largo2;
+ar = (char *)malloc(largototal + 1);
+if (ar != NULL)
+{
+for (i = 0; i < largo1; i++)
+{
+ar[i] = s1[i];
+}
+k = 0;
+for (j = i; j <= largototal; j++)
+{
+ar[j] = s2[k];
+k++;
+}
+return (ar);
+}
+else
+{
+return (NULL);
+}
 }
