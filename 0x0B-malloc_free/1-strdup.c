88a8e4200c86db368d3dc5190fa4ba60f20f0343
facu2279 <2279@holbertonschool.com>
2020-10-14 17:26:06 +0000

    0x0B

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,45 +3,43 @@
 /**
  * _strdup - sadf
  * @str: sadf
- * Return : sadf
+ * Return: sadf
  */
 char *_strdup(char *str)
 {
-  char *ar;
-  int size, i;
+char *ar;
+int size, i;
 
-  if (str != NULL)
-    {
-      for (i = 0; str[i] != '\0'; i++)
-	{
-	}
-    }
-  else
-    {
-      return (NULL);
-    }
-  i++;
-  size = i;
-
-  if (size == 0)
-    {
-      return (NULL);
-    }
-  else
-    {
-      ar = (char *)malloc(size);
-      
-      if (ar == NULL)
-	{
-	  return (NULL);
-	}
-      else
-	{
-	  for (i = 0; i <= size; i++)
-	    {
-	      ar[i] = str[i];
-	    }
-	  return (ar);
-	}
-    }
+if (str != NULL)
+{
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+}
+else
+{
+return (NULL);
+}
+i++;
+size = i;
+if (size == 0)
+{
+return (NULL);
+}
+else
+{
+ar = (char *)malloc(size);
+if (ar == NULL)
+{
+return (NULL);
+}
+else
+{
+for (i = 0; i <= size; i++)
+{
+ar[i] = str[i];
+}
+return (ar);
+}
+}
 }
