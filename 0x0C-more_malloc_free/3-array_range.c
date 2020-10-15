2658d5844081b1804c583756bd8ed5547802dd5a
facu2279 <2279@holbertonschool.com>
2020-10-15 21:11:42 +0000

    0X0C

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, tamano;
+	if (min > max)
+		return (NULL);
+	tamano = max - min + 1;
+	arr = malloc(tamano * 4);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i <= max; i++)
+	{
+		arr[i] = min;
+		min++;
+	}
+	return (arr);
+}
