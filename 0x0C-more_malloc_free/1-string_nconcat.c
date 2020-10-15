3fdcd70c96ec61c128bead05ba56ce603852cfa5
facu2279 <2279@holbertonschool.com>
2020-10-15 15:36:14 +0000

    0X0C

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - sdafa
+ * @s1: sadfa
+ * @s2: sdf
+ * @n: safd
+ * Return: sadfa
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ar;
+	unsigned int largo1, largo2, i, j, k;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (largo1 = 0; s1[largo1] != '\0'; largo1++)
+	{
+	}
+	for (largo2 = 0; s2[largo2] != '\0'; largo2++)
+	{
+	}
+	ar = (char *)malloc(largo1 + n);
+	if (ar != NULL)
+	{
+		for (i = 0; i < largo1; i++)
+		{
+			ar[i] = s1[i];
+		}
+		k = 0;
+		for (j = i; j < largo1 + n; j++)
+		{
+			ar[j] = s2[k];
+			k++;
+		}
+		return (ar);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
