2b99b6b3729b6e5a84f65e4cbd48a38602645212
facu2279 <70913784+facu2279@users.noreply.github.com>
2020-10-15 21:22:03 -0300

    asd

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,2 +1,21 @@
-#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: count elements
+ * @argv: elements
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	long int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (0);
+}
