94de694bca35109fab3f7d05acf7a96ff0c82842
facu2279 <70913784+facu2279@users.noreply.github.com>
2020-10-15 21:26:11 -0300

    a

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,35 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - entry point
- * @argc: count elements
- * @argv: elements
- * Return: always 0
+ * main - asdfas
+ * @argc: asdfas
+ * @argv: asdfas
+ * Return: asasd
  */
-
 int main(int argc, char *argv[])
 {
-	long long int num1, num2, res;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	num1 = atoi(argv[1]);
-	if (num1 == 0 && *argv[1] != 48)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	num2 = atoi(argv[2]);
-	if (num2 == 0 && *argv[2] != 48)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	res = num1 * num2;
-	printf("%ld\n", res);
-	return (0);
 }
