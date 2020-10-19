2905cab2ab5c6ced695753d4fbec7983de8430fb
facu2279 <2279@holbertonschool.com>
2020-10-19 16:28:33 +0000

    0x0D-Preprocessor

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: dfghdfgh
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
