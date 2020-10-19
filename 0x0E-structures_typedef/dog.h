85fbad5e8385393d4a44680cae31962669ab12b7
facu2279 <2279@holbertonschool.com>
2020-10-19 19:21:06 +0000

    0x0D-Preprocessor

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -31,4 +31,6 @@ char *owner;
 typedef struct dog_t dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
