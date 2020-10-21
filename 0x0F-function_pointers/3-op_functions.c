15185e37a366b502abb104b15000c605585c462f
facu2279 <2279@holbertonschool.com>
2020-10-21 20:32:57 +0000

    FUNCTION_POINTERS

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+/**
+ * op_add - func that sums 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the sum of the numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return the rest of the 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the resta of 2 numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the multiply of 2 numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - the division of 2 numbers
+ * @a: number 1
+ * @b: num 2
+ * Return: the division of 2 numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - return the mod of 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the mod of 2 numbers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
