a0ec8b1fbe8ceaac73dfaa722356ab5f4cda8e5e
facu2279 <2279@holbertonschool.com>
2020-10-21 21:31:22 +0000

    FUNCTION_POINTERS

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,21 +10,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	unsigned char *pf;
+	int xd;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	bytes = atoi(argv[1]);
+	xd = atoi(argv[1]);
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	i = 0;
 
 
 }
