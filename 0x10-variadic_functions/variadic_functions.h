ec08405269e163511eac79fdf33a326de3137861
facu2279 <2279@holbertonschool.com>
2020-10-24 19:18:34 +0000

    variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTIONS_H
-#define FUNCIONS_H
+#define FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
