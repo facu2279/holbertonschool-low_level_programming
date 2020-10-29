3e8f6432f7c8673693b19ab33d2bbe926718e30e
facu2279 <2279@holbertonschool.com>
2020-10-29 17:16:50 +0000

    0x012 Singly Linked Lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,9 +9,3 @@ void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
