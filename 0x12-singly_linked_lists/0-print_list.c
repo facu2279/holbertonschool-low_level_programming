3eb09525594fe9b4721b0a123d989f7aa955624b
facu2279 <2279@holbertonschool.com>
2020-10-29 16:50:03 +0000

    0x012 Singly Linked Lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,6 +8,7 @@
 size_t print_list(const list_t *h)
 {
 	int i = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -21,5 +22,5 @@ size_t print_list(const list_t *h)
 		i = i + 1;
 		h = h->next;
 	}
-	return(i);
+	return (i);
 }
