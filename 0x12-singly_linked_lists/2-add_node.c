f9199b263979af50cfda906549232924bebac857
facu2279 <2279@holbertonschool.com>
2020-10-29 17:14:45 +0000

    0x012 Singly Linked Lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * add_node - add a new node in a list
+ * @head: pointer received
+ * @str: constant string received
+ *Return: returns a new list with the added element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+}
