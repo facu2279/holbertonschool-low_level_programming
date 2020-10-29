27a0b07c19d515721620f278f16297c5de41e1f9
facu2279 <2279@holbertonschool.com>
2020-10-29 21:57:03 +0000

    0x012 Singly Linked Lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,19 @@
-howglobal main
-	        section .text
+%define sys_write 1
+	%define stdout 1
+	%define sys_exit 60
+	%define success 0
+	%define nl 10
+	section .data
+	    message db "Hello, Holberton", nl
+	    message_len equ $-message
+	section .text
+	global main
 main:
-	        mov     rax, 1
-	        mov     rdi, 1
-	        mov     rsi, msg
-	        mov     rdx, 18
-	        syscall
-	        mov     rax, 60
-	        mov     rdi, 0
-	        syscall
-	        section .data
-	        msg db "Hello, Holberton\n", 10
+	    mov rax, sys_write
+	    mov rdi, stdout
+	    mov rsi, message
+	    mov rdx, message_len
+	    syscall
+	    mov rax, sys_exit
+	    mov rdi, success
+	    syscall
