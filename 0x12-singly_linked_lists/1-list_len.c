cba2bae94353d2620d04bc097ed0b768629ce952
facu2279 <2279@holbertonschool.com>
2020-10-29 16:59:38 +0000

    0x012 Singly Linked Lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,21 @@
 #include "lists.h"
+#include <stdio.h>
 /**
- * list_len - return the number of elemnts in list
+ * list_len - return the number of elements in list
  * @h: pointer received
  *Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
+	int i = 0;
 
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			h = h->next;
+		}
+		i = i + 1;
+	}
+	return (i);
 }
