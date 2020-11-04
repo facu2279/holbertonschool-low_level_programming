e908d6f9996200f6c5f219d764bdd07e13b27a46
facu2279 <2279@holbertonschool.com>
2020-11-04 17:19:25 +0000

    more singly linked list day two

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * reverse_listint - reverse a linked list
+ * @head: pointer to pointer to starting a list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+
+}
