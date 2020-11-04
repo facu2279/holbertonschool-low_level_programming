77abb496ff5704c873b8225dfe3356d52d5da33e
facu2279 <2279@holbertonschool.com>
2020-11-04 16:54:49 +0000

    more singly linked list day two

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,22 +9,36 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+
 	unsigned int i;
 	listint_t *aux, *new;
 
-	if (*head == NULL)
-		return (0);
-	aux = *head;
-	for (i = 0; i < (idx - 1) && aux != NULL; i++)
+	if (head == NULL)
 	{
-	aux = aux->next;
-}
+		return (NULL);
+	}
 	new = malloc(sizeof(listint_t));
 	if (!new)
 	{
-	free(new);
-	return (0);
-}
+		free(new);
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	aux = *head;
+	for (i = 0; i < (idx - 1) && aux != NULL; i++)
+	{
+		aux = aux->next;
+	}
+	if (aux == NULL)
+	{
+		return (NULL);
+	}
 	new->n = n;
 	new->next = aux->next;
 	aux->next = new;
