a7da3f32df2d63fdebe146f1d8b21f56745093cf
facu2279 <70913784+facu2279@users.noreply.github.com>
2020-11-05 20:34:18 -0300

    fixx 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,10 @@
 #include "holberton.h"
+
 /**
- * binary_to_uint - convert binary to decimal
- * @b: char received
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
 unsigned int binary_to_uint(const char *b)
 {
