c4e4fc8a3e5956ec8b4b973e5ec9dbc6dc9aa8db
facu2279 <2279@holbertonschool.com>
2020-11-09 19:15:14 +0000

    0x15 - file I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * append_text_to_file - add text in the end of a file
+ * @filename: name of a file
+ * @text_content: content of a file
+ * Return: 1 success or -1 fail
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
