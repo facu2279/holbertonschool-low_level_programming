2ee092019cdcb95647099713f069e3f8432de822
facu2279 <2279@holbertonschool.com>
2020-11-09 17:42:04 +0000

    0x15 - file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,13 +7,10 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+
 	int fd, i, len;
 	char *buf;
 
-	if (filename == NULL)
-	{
-		return (0);
-	}
 	buf = malloc(letters + 1);
 
 	fd = open(filename, O_RDONLY, 0600);
@@ -24,16 +21,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	len = read(fd, buf, letters);
-	if (len < 1)
-	{
-		return (0);
-	}
 
 	i = write(STDOUT_FILENO, buf, len);
-	if (i < 0)
-	{
-		return (0);
-	}
+
 	close(fd);
 
 	return (i);
