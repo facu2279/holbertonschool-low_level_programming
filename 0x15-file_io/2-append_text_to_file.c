4c5085813e217f21b43015d57af66c23bcb07541
facu2279 <2279@holbertonschool.com>
2020-11-09 19:33:39 +0000

    0x15 - file I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,10 +1,29 @@
 #include "holberton.h"
 /**
- * append_text_to_file - add text in the end of a file
- * @filename: name of a file
- * @text_content: content of a file
- * Return: 1 success or -1 fail
+ *append_text_to_file - append text_content to filename
+ *@filename: name of file
+ *@text_content: text inside file
+ *Return: 1 on success, -1 failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fo, fw, i = 0;
+
+	if (!filename)
+		return (-1);
+
+	fo = open(filename, O_APPEND | O_WRONLY);
+	if (fo == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+		fw = write(fo, text_content, i);
+		if (fw == -1)
+			return (-1);
+	}
+	close(fo);
+	return (1);
 }
