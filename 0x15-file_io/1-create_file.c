9d7562655968b59dec9328e975fbd60cc225a540
facu2279 <2279@holbertonschool.com>
2020-11-09 18:48:13 +0000

    0x15 - file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,9 +31,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	i = length_(text_content);
-	text_content[i + 1] = '\0';
-	write(fd, text_content, i);
-
+	if (i > 0)
+	{
+		text_content[i + 1] = '\0';
+		write(fd, text_content, i);
+	}
 	close(fd);
 	return (1);
 }
