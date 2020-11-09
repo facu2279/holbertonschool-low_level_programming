e1962c5569ec58c478b441dafd2543ffd0049d74
facu2279 <2279@holbertonschool.com>
2020-11-09 19:02:20 +0000

    0x15 - file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,6 +25,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		return (-1);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 00600);
 	if (fd == -1)
@@ -33,7 +35,6 @@ int create_file(const char *filename, char *text_content)
 	i = length_(text_content);
 	if (i > 0)
 	{
-		text_content[i + 1] = '\0';
 		write(fd, text_content, i);
 	}
 	close(fd);
