19fd6363be546e85cc258bbab0c1c8ce1fb0dc6d
facu2279 <2279@holbertonschool.com>
2020-11-09 17:28:00 +0000

    0x15 - file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,29 @@
-#include "holberton.h" 1-create_file.c 2-append_text_to_file.c 3-cp.c
+#include "holberton.h"
+/**
+ * read_textfile - this function read text on someone file
+ * @filename: name of file
+ * @letters: the number of length of text
+ * Return: ssize_t structure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, i, len;
+	char *buf;
+
+	buf = malloc(letters + 1);
+
+	fd = open(filename, O_RDONLY, 0600);
+
+	if (fd == -1)
+	{
+		return(1);
+	}
+
+	len = read(fd, buf, letters);
+
+	i = write(1, buf, len);
+
+	close(fd);
+
+	return(i);
+}
