0d81eac227132729c1009bf98a7467e5edf33c3e
facu2279 <2279@holbertonschool.com>
2020-12-12 19:16:51 +0000

    edit gitignore and add task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,40 @@
+#include "lists.h"
+/**
+* insert_dnodeint_at_index - insert node in specific index
+* @h: pointer to list
+* @idx: index
+* @n: value of new node
+* Return: list with new node
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    
+unsigned int i;
+dlistint_t *copy;
+dlistint_t *aux;
+dlistint_t *new;
+
+if (h == NULL)
+{
+return (add_dnodeint(h, n));
+}
+copy = *h;
+for (i = 0; i < idx && copy != NULL; i++)
+{
+copy = copy->next;
+}
+if (copy == NULL)
+{
+return (add_dnodeint_end(h, n));
+}
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+copy = copy->prev;
+aux = copy->next;
+new->n = n;
+copy->next = new;
+new->prev = copy;
+aux->prev = new;
+new->next = aux;
+return (copy);
 }
