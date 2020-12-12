7070de53e5929613fd3fbc3175ed5cef0f3ea1b8
facu2279 <2279@holbertonschool.com>
2020-12-12 15:40:54 +0000

    fix task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,31 @@
-size_t dlistint_len(const dlistint_t *h)
+#include "lists.h"
+/**
+* add_dnodeint - add node in a lsit
+* @head: pointer to double list
+* @n: const integer
+* Return: a new list
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    
+dlistint_t *newnode;
+
+newnode = malloc(sizeof(dlistint_t));
+if (newnode == NULL)
+{
+return (NULL);
+}
+newnode->n = n;
+newnode->prev = NULL;
+if (*head == NULL)
+{
+newnode->next = NULL;
+*head = newnode;
+}
+else
+{
+newnode->next = *head;
+(*head)->prev = newnode;
+*head = newnode;
+}
+return (newnode);
 }
