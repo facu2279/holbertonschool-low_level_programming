bf86ebcfaa34a0720028aa5475448a1ed7ad55cd
facu2279 <2279@holbertonschool.com>
2020-12-12 16:48:55 +0000

    add task 4 free

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,4 +1,16 @@
+#include "lists.h"
+/**
+* free_dlistint - free list
+* @head: pointer to list
+*/
 void free_dlistint(dlistint_t *head)
 {
-    
+    dlistint_t *tmp;
+
+    while (head != NULL)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
 }
