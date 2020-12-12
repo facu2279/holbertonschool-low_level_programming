fdd88c075bd4a77c550b483a11acf9b3d3b97b17
facu2279 <2279@holbertonschool.com>
2020-12-12 17:50:57 +0000

    fix betty

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,55 +1,55 @@
 #include "lists.h"
 /**
 * get_dnodeint_at_index - search value of index and return list
+* @head: pointer to list
+* @index: index
+* Return: list
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i;
-    dlistint_t *tmp;
+unsigned int i;
+dlistint_t *tmp;
 
-    tmp = head;
-    for (i = 0; i < index && tmp != NULL; i++)
-    {
-        tmp = tmp->next;
-    }
-    /*if (index > i)
-        tmp = NULL;*/
-    return (tmp);
-}
-
-/*
-    largo = dlistint_len(head);
-    aux = head;
-    printf("\n el largo es%i y el index es%i\n", largo, index);
-    if (index < largo)
-    {
-        printf("entro");
-        
-        for (i = 0; i <= largo; i++)
-        {
-            aux = aux->next;
-        }
-        if (i != 0)
-            return (aux);
-        else
-            return NULL;
-    }
-    else
-    {
-        return NULL;
-        printf("index mayor que largo");
-        return (NULL);
-    }
-}
-size_t dlistint_len(const dlistint_t *h)
+tmp = head;
+for (i = 0; i < index && tmp != NULL; i++)
 {
-int i = 0;
-
-while (h)
-{
-i++;
-h = h->next;
+tmp = tmp->next;
 }
-return (i);
+return (tmp);
 }
+/**
+* OLD FUNCTION - XDXD
+   * largo = dlistint_len(head);
+   * aux = head;
+   * printf("\n el largo es%i y el index es%i\n", largo, index);
+   * if (index < largo)
+   * {
+    *    printf("entro");
+      *  for (i = 0; i <= largo; i++)
+      *  {
+       *     aux = aux->next;
+       * }
+       * if (i != 0)
+       *     return (aux);
+       * else
+    *return NULL;
+    *}
+    *else
+    *{
+    *    return NULL;
+    *    printf("index mayor que largo");
+    *    return (NULL);
+    *}
+*}
+*size_t dlistint_len(const dlistint_t *h)
+*{
+*int i = 0;
+*
+*while (h)
+*{
+*i++;
+*h = h->next;
+*}
+*return (i);
+*}
 */
