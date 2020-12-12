9b13a0d4fff9bb3fe2e3b0695a35b0a8c3a39325
facu2279 <2279@holbertonschool.com>
2020-12-12 18:10:38 +0000

    add task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,4 +1,17 @@
+#include "lists.h"
+/**
+* sum_dlistint - sum all elements of a list
+* @head: pointer to list
+* Return: sum of all elements
+*/
 int sum_dlistint(dlistint_t *head)
 {
-    
+int sum;
+
+while (head)
+{
+sum = sum + head->n;
+head = head->next;
+}
+return (sum);
 }
