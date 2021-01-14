6a4b86c66d1de809535f85b74843dfdb3d0d4854
facu2279 <2279@holbertonschool.com>
2021-01-14 05:05:37 +0000

    Add task 0 - betty checked

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,8 @@
+/*
+ * task 0-hash_table_create.c
+ * Made by Facundo Diaz to Holberton School
+ * Date - 14/01/2021
+ */
 #include "hash_tables.h"
 /**
 * hash_table_create - creates a hash table
@@ -6,5 +11,26 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new_ht;
+	unsigned long int i;
 
+	if (size <= 0)
+		return (NULL);
+
+	new_ht = malloc(sizeof(hash_table_t));
+	if (new_ht == NULL)
+		return (NULL);
+
+	new_ht->array = malloc(sizeof(hash_node_t) * size);
+	if (new_ht->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		new_ht->array[i] = NULL;
+	}
+
+	new_ht->size = size;
+
+	return (new_ht);
 }
