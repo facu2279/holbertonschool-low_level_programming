fcafdb8aaeeb17675943a2bf17d46c4f76090072
facu2279 <2279@holbertonschool.com>
2021-01-14 05:30:54 +0000

    Add comments in task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,14 @@
+/*
+ * task 6-hash_table_delete.c
+ * Made by Facundo Diaz to Holberton School
+ * Date - 14/01/2021
+ */
+#include "hash_tables.h"
+/**
+* hash_table_delete - deletes a hash table.
+* @ht: is the hash table
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+
+}
