e864c6a809b1c8c26044b31ca17772060255a079
facu2279 <2279@holbertonschool.com>
2021-01-14 05:26:54 +0000

    Add comments in task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,17 @@
+/*
+ * task 4-hash_table_get.c
+ * Made by Facundo Diaz to Holberton School
+ * Date - 14/01/2021
+ */
+#include "hash_tables.h"
+/**
+* hash_table_get - retrieves a value associated with a key
+* @ht: is the hash table you want to look into
+* @key: is the key
+* Return: Returns the value associated with the element, or NULL
+* if key couldn’t be found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+}
