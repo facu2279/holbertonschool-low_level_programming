13423f8b73fda1ade2666d6d57c528bf08eac64f
facu2279 <2279@holbertonschool.com>
2021-01-14 05:24:17 +0000

    Add comments in task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,17 @@
+/*
+ * task 3-hash_table_set.c
+ * Made by Facundo Diaz to Holberton School
+ * Date - 14/01/2021
+ */
+#include "hash_tables.h"
+/**
+* hash_table_set - gives you the index of a key
+* @ht: is the hash table you want to add or update the key/value to
+* @key: is the key
+* @value: value is the value associated with the key
+* Return:1 if it succeeded, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
