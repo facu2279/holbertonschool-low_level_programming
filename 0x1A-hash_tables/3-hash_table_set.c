5985f7309da267b9f263e428d4d68f12052cdf65
facu2279 <2279@holbertonschool.com>
2021-01-15 04:30:44 +0000

    Fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,42 +13,43 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node = NULL;
-	hash_node_t *aux_node = NULL;
+	hash_node_t *new_node, *ptr;
+	int index;
 
-	if (!key)
-	{
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
-	}
 	index = key_index((unsigned char *)key, ht->size);
-	aux_node = (ht->array)[index];
-	while (aux_node != NULL)
+	ptr = ht->array[index];
+	while (ptr != NULL)
 	{
-		if (strcmp(key, aux_node->key) == 0)
+		if (strcmp(ptr->key, key) == 0)
 		{
-			free(aux_node->value);
-			aux_node->value = strdup(value);
-			return (0);
+			free(ptr->value);
+			ptr->value = strdup(value);
+			return (1);
 		}
-		aux_node = aux_node->next;
+		ptr = ptr->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
 	{
+		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	if ((ht->array)[index] == NULL)
-	{
-		(ht->array)[index] = new_node;
-	}
+	new_node->next = NULL;
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_node;
 	else
 	{
-		new_node->next = (ht->array)[index];
-		(ht->array)[index] = new_node;
+		/* check if the key matches*/
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
 	}
 	return (1);
 }
