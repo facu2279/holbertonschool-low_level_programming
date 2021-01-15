5e2afdbe1a5ce4d703f959cd9e53c308fa606aa7
facu2279 <2279@holbertonschool.com>
2021-01-15 18:58:51 +0000

    Fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,5 +13,42 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    
+	unsigned long int index;
+	hash_node_t *new_node = NULL;
+	hash_node_t *aux_node = NULL;
+
+	if (!key || !value || !ht || strlen(key) == 0)
+	{
+		return (0);
+	}
+	index = key_index((unsigned char *)key, ht->size);
+	aux_node = (ht->array)[index];
+	while (aux_node != NULL)
+	{
+		if (strcmp(key, aux_node->key) == 0)
+		{
+			free(aux_node->value);
+			aux_node->value = strdup(value);
+			return (1);
+		}
+		aux_node = aux_node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if ((ht->array)[index] == NULL)
+	{
+		(ht->array)[index] = new_node;
+	}
+	else
+	{
+		new_node->next = (ht->array)[index];
+		(ht->array)[index] = new_node;
+	}
+	return (1);
 }
