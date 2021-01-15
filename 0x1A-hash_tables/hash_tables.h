4fcc46353082774320aea8496ccc50d88c717dab
facu2279 <2279@holbertonschool.com>
2021-01-15 03:22:06 +0000

    Final commit to this task

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -67,9 +67,6 @@ shash_node_t **array;
 shash_node_t *shead;
 shash_node_t *stail;
 } shash_table_t;
-/**
-* Mandatory tasks
-*/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -78,9 +75,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-/**
-* Advanced task functions
-*/
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
