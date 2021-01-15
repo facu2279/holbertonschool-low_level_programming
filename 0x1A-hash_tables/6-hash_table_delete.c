f7bf04a97aa01f38a7ad2091015279e4ab868871
facu2279 <2279@holbertonschool.com>
2021-01-15 03:49:58 +0000

    Check betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,22 +10,22 @@
 */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *aux;
-    hash_node_t *aux2;
-    unsigned long int i;
-    
-    for (i = 0; i < ht->size; i++)
-    {
-        aux = ht->array[i];
-        while (aux != NULL)
-        {   
-            aux2 = aux->next;
-            free(aux->value);
-            free(aux->key);
-            free(aux);
-            aux = aux2;
-        }
-    }
-    free(ht->array);
-    free(ht);
+hash_node_t *aux;
+hash_node_t *aux2;
+unsigned long int i;
+
+for (i = 0; i < ht->size; i++)
+{
+aux = ht->array[i];
+while (aux != NULL)
+{
+aux2 = aux->next;
+free(aux->value);
+free(aux->key);
+free(aux);
+aux = aux2;
+}
+}
+free(ht->array);
+free(ht);
 }
