a27721c707a62db1392410b95825f228a9c0bd70
facu2279 <2279@holbertonschool.com>
2021-01-15 03:13:20 +0000

    Fix task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,23 +12,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
-	unsigned long int i;
+	unsigned int i;
 
-	if (size <= 0)
+	if (size < 1)
 		return (NULL);
 
 	new_ht = malloc(sizeof(hash_table_t));
 	if (new_ht == NULL)
 		return (NULL);
 
-	new_ht->array = malloc(sizeof(hash_node_t) * size);
-	if (new_ht->array == NULL)
+	new_ht->array = malloc(sizeof(void *) * size);
+	if (new_ht == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-	{
 		new_ht->array[i] = NULL;
-	}
 
 	new_ht->size = size;
 
