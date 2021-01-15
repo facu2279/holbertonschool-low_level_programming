1f6e28e047975769f0d4cdc86a2738b65305d61d
facu2279 <2279@holbertonschool.com>
2021-01-15 04:19:56 +0000

    Fix betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,27 +10,27 @@
 */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *aux_node;
-	unsigned long int i;
-    char *separador = "";
+hash_node_t *aux_node;
+unsigned long int i;
+char *separador = "";
 
-	if (ht)
-    {
-    printf("{");
-	for (i = 0; i < ht->size; i++)
-	{
-		aux_node = ht->array[i];
-		while (aux_node != NULL)
-		{
-			printf("%s", separador);
-				separador = ", ";
+if (ht)
+{
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+aux_node = ht->array[i];
+while (aux_node != NULL)
+{
+printf("%s", separador);
+separador = ", ";
 
-				if (aux_node->key)
-					printf("'%s': '%s'", aux_node->key, aux_node->value);
+if (aux_node->key)
+printf("'%s': '%s'", aux_node->key, aux_node->value);
 
-			aux_node = aux_node->next;
-        }
-	}
-	printf("}\n");
-    }
+aux_node = aux_node->next;
+}
+}
+printf("}\n");
+}
 }
