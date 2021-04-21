2a497ff09bb2944d1b4e5deec06dc2f21e244f9d
facu2279 <2279@holbertonschool.com>
2021-04-21 22:23:04 +0000

    Fix betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,38 +9,38 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-  size_t i, i2, salto;
-  int pos;
+size_t i, i2, salto;
+int pos;
 
-  if (array)
-    {
-      salto = sqrt(size);
-      i = 0;
-      i2 = salto;
-      pos = array[i];
-      printf("Value checked array[%lu] = [%d]\n", i, pos);
-      while (i2 < size && pos < value)
-	{
-	  if (array[i2] >= value)
-	    break;
-	  i = i + salto;
-	  i2 = i2 + salto;
-	  pos = array[i];
-	  printf("Value checked array[%lu] = [%d]\n", i, pos);
-	}
-      if (i >= size || pos > value)
-	return (-1);
-      printf("Value found between indexes [%lu] and [%lu]\n", i, i2);
-      while (i <= calcmin(size - 1, i2) && pos <= value)
-	{
-	  pos = array[i];
-	  printf("Value checked array[%lu] = [%d]\n", i, pos);
-	  if (pos == value)
-	    return (i);
-	  i++;
-	}
-    }
-  return (-1);
+if (array)
+{
+salto = sqrt(size);
+i = 0;
+i2 = salto;
+pos = array[i];
+printf("Value checked array[%lu] = [%d]\n", i, pos);
+while (i2 < size && pos < value)
+{
+if (array[i2] >= value)
+break;
+i = i + salto;
+i2 = i2 + salto;
+pos = array[i];
+printf("Value checked array[%lu] = [%d]\n", i, pos);
+}
+if (i >= size || pos > value)
+return (-1);
+printf("Value found between indexes [%lu] and [%lu]\n", i, i2);
+while (i <= calcmin(size - 1, i2) && pos <= value)
+{
+pos = array[i];
+printf("Value checked array[%lu] = [%d]\n", i, pos);
+if (pos == value)
+return (i);
+i++;
+}
+}
+return (-1);
 }
 /**
  * calcmin - finds the minimum of two values
@@ -50,7 +50,7 @@ int jump_search(int *array, size_t size, int value)
  */
 size_t calcmin(size_t primero, size_t segundo)
 {
-  if (segundo < primero)
-    return (segundo);
-  return (primero);
+if (segundo < primero)
+return (segundo);
+return (primero);
 }
