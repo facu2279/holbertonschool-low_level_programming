f9b84508a1073bda6fb74ed5f530ab463a52e0e2
facu2279 <2279@holbertonschool.com>
2021-04-21 14:38:48 +0000

    finish

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array
+ * of integers using the Binary search algorithm
+ * Made by Facundo Diaz for Holberton School 2021
+ * @array: array to check
+ * @size: size of array
+ * @value: value to search
+ * Return: returns the index where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+  size_t left = 0, right = size - 1, mid, i;
+
+  if (array == NULL)
+    return (-1);
+
+  while (left <= right)
+    {
+      printf("Searching in array: ");
+      for (i = left; i <= (right - 1); i++)
+	{
+	  printf("%d, ", array[i]);
+	}
+      printf("%d\n", array[right]);
+      mid = (left + right) / 2;
+      if (array[mid] < value)
+	{
+	  left = mid + 1;
+	}
+      else if (array[mid] > value)
+	{
+	  right = mid - 1;
+	}
+      else
+	return (mid);
+    }
+  return (-1);
+ }
